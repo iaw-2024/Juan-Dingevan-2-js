f718195faaebf9cb24a0833cdcaf380162149118
Juan Dingevan <jidingevan@gmail.com>
2024-03-22 15:59:25 -0300

    Finished Server-side JS section.

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.get("/express", (req, res) => res.send("Express on Vercel!"));
-app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
-app.use(express.static('public'))
+app.get("/express", (req, res) => {
+    res.sendFile("./public/express/index.html", { root: '.' });
+});
 
+app.get("/cliente_servidor", (req, res) => {
+    res.sendFile("./public/cliente_servidor/index.html", { root: '.' });
+});
+
+app.get("/datos", (req, res) => {
+    res.sendFile("./public/datos.json", { root: '.' });
+});
+
+app.use(express.static('public'))
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
