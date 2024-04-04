4f623a28357b0f5752b4fe3aed6eea07ca31f3f0
Juan Dingevan <jidingevan@gmail.com>
2024-04-04 16:52:50 -0300

    Fixed express section

--- api/index.js
+++ api/index.js
@@ -1,8 +1,58 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 app.get("/express", (req, res) => {
-    res.sendFile("./public/express/index.html", { root: '.' });
+    fs.readFile("public/express/index.html", "utf8", (err, html) => {
+        if(err) {
+            console.log(err)
+            res.status(500).send("Error del servidor");
+            return;
+        }
+
+        fs.readFile("./public/datos.json", "utf8", (err2, datosCrudos) => {
+            if(err2) {
+                console.log(err2)
+                res.status(500).send("Error 2 del servidor");
+                return;
+            }
+
+            let datos = JSON.parse(datosCrudos);
+
+            let tableHTML = '<table class="w-full border-collapse border shadow-lg bg-white">';
+            tableHTML += '<thead><tr class="bg-green-600 text-white">';
+            tableHTML += '<th class="py-3 px-6 text-left border-b border-black-200">Tostadores</th>';
+            tableHTML += '<th class="py-3 px-6 text-left border-b border-black-200">País de Origen</th>';
+            tableHTML += '<th class="py-3 px-6 text-left border-b border-black-200">Región de Origen</th>';
+            tableHTML += '<th class="py-3 px-6 text-left border-b border-black-200">Varietales</th>';
+            tableHTML += '<th class="py-3 px-6 text-left border-b border-black-200">Proceso</th>';
+            tableHTML += '<th class="py-3 px-6 text-left border-b border-black-200">Notas de Cata</th>';
+            tableHTML += '<th class="py-3 px-6 text-left border-b border-black-200">Peso (gr)</th>';
+            tableHTML += '<th class="py-3 px-6 text-left border-b border-black-200">Precio (USD)</th>';
+            tableHTML += '</tr></thead><tbody>';
+
+            datos.forEach(cafe => {
+                tableHTML += '<tr class="border-b border-gray-200">';
+                tableHTML += `<td class="py-3 px-6 text-left whitespace-nowrap">${cafe['tostadores']}</td>`;
+                tableHTML += `<td class="py-3 px-6 text-left whitespace-nowrap">${cafe['paisDeOrigen']}</td>`;
+                tableHTML += `<td class="py-3 px-6 text-left whitespace-nowrap">${cafe['regionDeOrigen']}</td>`;
+                tableHTML += `<td class="py-3 px-6 text-left whitespace-nowrap">${cafe['varietales']}</td>`;
+                tableHTML += `<td class="py-3 px-6 text-left whitespace-nowrap">${cafe['proceso']}</td>`;
+                tableHTML += `<td class="py-3 px-6 text-left whitespace-nowrap">${cafe['notasDeCata']}</td>`;
+                tableHTML += `<td class="py-3 px-6 text-left whitespace-nowrap">${cafe['peso']}</td>`;
+                tableHTML += `<td class="py-3 px-6 text-left whitespace-nowrap">${cafe['precio']}</td>`;
+                tableHTML += '</tr>';
+            });
+
+            tableHTML += '</tbody></table>';
+
+            console.log(tableHTML);
+
+            const htmlNew = html.replace("<TABLA />", tableHTML);
+
+            res.send(htmlNew);
+        })
+    })
 });
 
 app.get("/cliente_servidor", (req, res) => {
