3a859ce95ce4b500148d0c118af9ece144d37ed1
Juan Dingevan <jidingevan@gmail.com>
2024-04-05 14:40:30 -0300

    Trying to successfully deploy express endpoint #2

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const process = require("process");
 const app = express();
 
 app.get("/express", (req, res) => {
-    fs.readFile("public/express/index.html", "utf8", (err, html) => {
+    /*fs.readFile("public/express/index.html", "utf8", (err, html) => {
         if(err) {
             console.log(err)
             res.status(500).send("Error del servidor");
@@ -54,7 +54,8 @@ app.get("/express", (req, res) => {
 
             res.send(datosCrudos);
         })
-    })
+    })*/
+    res.send("pepe");
 });
 
 app.get("/cliente_servidor", (req, res) => {
